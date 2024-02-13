bab33a4b5f6030b69dcece87e024b88fde1b66a5
Rodney Lab <codesign@rodneylab.com>
2024-02-13 05:22:34 +0000

    build(dependencies): 🧱 update packages
    ┌───────────────────────────────────────┬─────────┬────────┐
    │ Package                               │ From    │ To     │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ eslint (dev)                          │ 8.44.0  │ 8.56.0 │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ svelte (dev)                          │ 4.0.5   │ 4.2.10 │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ svelte-check (dev)                    │ 3.4.5   │ 3.6.4  │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ @commitlint/config-conventional (dev) │ 17.6.6  │ 18.6.0 │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ @sveltejs/kit (dev)                   │ 1.22.1  │ 2.5.0  │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ commitlint (dev)                      │ 17.6.6  │ 18.6.0 │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ eslint-config-prettier (dev)          │ 8.8.0   │ 9.1.0  │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ husky (dev)                           │ 8.0.3   │ 9.0.10 │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ prettier (dev)                        │ 2.8.8   │ 3.2.5  │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ prettier-plugin-svelte (dev)          │ 2.10.1  │ 3.1.2  │
    ├───────────────────────────────────────┼─────────┼────────┤
    │ vite (dev)                            │ 4.4.2   │ 5.1.1  │
    └───────────────────────────────────────┴─────────┴────────┘

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [vitePreprocess({})],
 	kit: {},
 };
 
